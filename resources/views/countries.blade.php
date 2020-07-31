5730cca4df6c3d1337e4ed9715415de8f5215d38
cassandra645 <meursault45@live.com>
2020-07-31 12:28:32 +0200

    change the appearance of the country-cards

--- resources/views/countries.blade.php
+++ resources/views/countries.blade.php
@@ -3,18 +3,20 @@
 <h1  style="text-align: center">Countries List</h1>
 <ul class="countries-list d-flex flex-row flex-wrap col-12 p-3">
     @foreach($data as $datas)
-    <li class="country-item card col-3 m-5 p-0">
+    <li class="country-item card col-3 m-5 p-0" style="background-color: #16191a; color: #ffffff; border:none">
         <div class="card-header">
-            <h3 class="country-name">{{ $datas['name'] }}</h3>
+            <h3 class="country-name" style="text-align: center">{{ $datas['name'] }}</h3>
         </div>
         <div class="card-img">
-            <img src="{{ $datas['flag'] }}" alt="country flag" style="width: 100%">
+            <img src="{{ $datas['flag'] }}" alt="country flag" class="img-fluid">
         </div>
-        <div class="card-body">
-            <p class="country-region">Region : {{ $datas['region'] }}</p>
+        <div class="card-body p-0" style="background-color: #313638;" >
+            <p class="country-region ">Region : {{ $datas['region'] }}</p>
             <p class="country-subregion">Subregion : {{ $datas['subregion'] }}</p>
-            <p class="country-capital">Capital : {{ $datas['capital'] }}</p>
-            <p class="population">Population : {{ $datas['population'] }}</p>
+            <ul class="list-group list-group-flush p-0">
+                <li class="list-group-item country-capital" style="background-color: #1e2224">Capital : {{ $datas['capital'] }}</li>
+                <li class="list-group-item population" style="background-color: #1e2224">Population : {{ $datas['population'] }}</li>
+            </ul>
         </div>
     </li>
     @endforeach
