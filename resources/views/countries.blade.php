c67d86e9b9af36a4da1a4af4440e1e770a43733b
Lascoyle <meursault45@live.com>
2021-05-29 15:36:27 +0200

    Reinsert the subregion

--- resources/views/countries.blade.php
+++ resources/views/countries.blade.php
@@ -13,7 +13,7 @@
         <div class="card-body p-0" style="background-color:#16191a; color: #ffffff" >
             <div class="card-text p-4 pt-5" style="background-color: #ffffff; color: #16191a; font-size: 1.3em">
                 <p class="country-region">Region : {{ $datas['region'] }}</p>
-                <p class="country-subregion">Subregion : {{ $datas['subregion'] }}</p>
+                <p class="country-subregion">Subregion : {{ $datas['subregion']}}</p>
             </div>
             <ul class="list-group list-group-flush p-0">
                 <li class="list-group-item country-capital" style="background-color: #1e2224">Capital : {{ $datas['capital'] }}</li>
